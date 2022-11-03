b127e368c70ba001f968985b97920fdd93437bf5
Victoria Niu <57949035+victoriacooperworld@users.noreply.github.com>
2022-11-02 17:08:16 -0700

    Added M4 but still need to figure out the okay

--- M4/UDPServer.java
+++ M4/UDPServer.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -31,26 +34,32 @@ public class UDPServer {
                 DatagramPacket packet2=new DatagramPacket(data2, data2.length, address, port);
                 //3.响应客户端
                 socket.send(packet2);
+            }else if (info.startsWith("get ")){
+                InetAddress address=packet.getAddress();
+                int port=packet.getPort();
+                String res = getFile(info);
+                if (!res.equals("No such file exist!")){
+                    // byte[] okay = "ok".getBytes();
+                    // DatagramPacket packet2=new DatagramPacket(okay, okay.length, address, port);
+                    // socket.send(packet2);
+                    byte[] data2=res.getBytes();
+                    DatagramPacket packet3=new DatagramPacket(data2, data2.length, address, port);
+                    socket.send(packet3);
+                }
             }else{
                 InetAddress address=packet.getAddress();
                 int port=packet.getPort();
-                byte[] data2="Welcome".getBytes();
-                //2.创建数据报，包含响应的数据信息
+                byte[] data2="haha".getBytes();
                 DatagramPacket packet2=new DatagramPacket(data2, data2.length, address, port);
-                //3.响应客户端
                 socket.send(packet2);
-            }
-            
+            } 
         }
-        
-        //4.关闭资源
-        // socket.close();
     }
     public static String getIndex(String dir){
         File dirPath = new File(dir);
         File[] files = dirPath.listFiles();
         if (files.length == 0){
-            return "EMPTY";
+            return "Empty directory!";
         }else{
             System.out.println("Getting the indexes...");
             StringBuilder sb = new StringBuilder();
@@ -60,5 +69,32 @@ public class UDPServer {
             }
             return sb.toString();
         }
+    }
+
+    public static String getFile(String cmd) throws FileNotFoundException{
+        File dirPath = new File("M4/data");
+        File[] files = dirPath.listFiles();
+        String fileName = cmd.substring(4,cmd.length());
+        System.out.println("server>filename: "+fileName);
+        for (File file:files){
+            if (file.getName().equals(fileName)){
+                FileReader fileReader = new FileReader(file);
+                try (BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+                    StringBuilder sb = new StringBuilder();
+                    String line = bufferedReader.readLine();
+                    while (line!=null){
+                        sb.append(line);
+                        sb.append('\n');
+                        line = bufferedReader.readLine();
+                        }
+                    return sb.toString();
+                } catch (FileNotFoundException e) {
+                    throw e;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }      
+        }
+        return "No such file exist!";
     }
 }
--- M4/UdpClient.java
+++ M4/UdpClient.java
@@ -14,24 +14,14 @@ public class UdpClient {
         while(true){
             String inp = sc.nextLine();
             byte[] data=inp.getBytes();
-            //2.创建数据报，包含发送的数据信息
             DatagramPacket packet=new DatagramPacket(data, data.length, address, port);
-            //3.创建DatagramSocket对象
             DatagramSocket socket=new DatagramSocket();
-            //4.向服务器端发送数据报
             socket.send(packet);
-
-            /*
-            * 接收服务器端响应的数据
-            */
-            //1.创建数据报，用于接收服务器端响应的数据
             byte[] data2=new byte[1024];
             DatagramPacket packet2=new DatagramPacket(data2, data2.length);
-            //2.接收服务器响应的数据
             socket.receive(packet2);
-            //3.读取数据
             String reply=new String(data2, 0, packet2.getLength());
-            System.out.println("我是客户端，服务器说："+reply);
+            System.out.println("Server: "+reply);
         }
         
         //4.关闭资源
